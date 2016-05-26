e37a26500d5e5e58fa808f2d7607d2865cc8b817
SuzyWu2014 <shujinwu@onid.oregonstate.edu>
2016-05-26 13:39:21 -0700

    more openstack preparing steps

--- openStack-pre.sh
+++ openStack-pre.sh
@@ -2,8 +2,11 @@
 passwd
 # allow root login with password
 vi /etc/ssh/sshd_config
-
 #PermitRootLogin yes
 # PasswordAuthentication yes
-
 service sshd restart
+
+# turn off docker0 interface
+systemctl stop docker
+ip link set dev docker0 down
+brctl delbr docker0
